f4b56c693bb39fca9747ea6cf84520d348a440cc
Nélio Dias <jrneliodias@gmail.com>
2024-05-26 08:09:09 -0300

    feat: clerk auth authenticantion

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,24 @@
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+
+export default function Page() {
+    return (
+        <div className=" min-h-screen grid grid-cols-1 lg:grid-cols-2">
+
+            <div className="h-full bg-slate-900  flex flex-col items-center justify-center px-4">
+                <ClerkLoaded>
+                    <SignIn path="/sign-in" />
+                </ClerkLoaded>
+                <ClerkLoading>
+                    <Loader2 className="w-6 h-6 animate-spin" />
+                </ClerkLoading>
+            </div>
+
+            <div className="h-full bg-slate-600 hidden lg:flex items-center justify-center">
+                <Image src="/logoipsum-226.svg" alt="login" width={500} height={500} />
+            </div>
+        </div>
+
+    );
+}
