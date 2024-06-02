400f313775c2fc40a5d5a8e386c2bcb016ca651e
Nélio Dias <jrneliodias@gmail.com>
2024-06-02 09:15:48 -0300

    chore: fix the drizzle setup to neon

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,10 +1,8 @@
-import 'dotenv/config'
-
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 
 const connectionString = process.env.DATABASE_URL!
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
-export const client = postgres(connectionString, { prepare: false })
+export const client = neon(connectionString)
 export const db = drizzle(client);
