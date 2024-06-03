9a154def7f3be04356e279c88bf4438d5aaa9370
Nélio Dias <jrneliodias@gmail.com>
2024-06-02 21:10:50 -0300

    feat: implement get and post routes for account

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,7 +2,6 @@ import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import accounts from './accounts';
-import { HTTPException } from 'hono/http-exception';
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
@@ -11,13 +10,7 @@ app.use('*', clerkMiddleware())
 const routes = app
     .route('/accounts', accounts)
 
-app.onError((err, c) => {
-    if (err instanceof HTTPException) {
-        return err.getResponse()
-    }
 
-    return c.json({ error: "Internal Server Error" }, 500)
-})
 
 export const GET = handle(app)
 export const POST = handle(app)
