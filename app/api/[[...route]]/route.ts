4708805cd4ce3cd8aa03a8c4f808126e919606d2
Nélio Dias <jrneliodias@gmail.com>
2024-06-23 14:30:01 -0300

    feat: create Transactions API routes

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,6 +3,7 @@ import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import accounts from './accounts';
 import categories from './categories';
+import transactions from './transactions';
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
@@ -11,6 +12,7 @@ app.use('*', clerkMiddleware())
 const routes = app
     .route('/accounts', accounts)
     .route('/categories', categories)
+    .route('/transactions', transactions)
 
 
 
